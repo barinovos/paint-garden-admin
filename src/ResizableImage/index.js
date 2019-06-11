516740dfd16110d0c480300d10b8e255f3d0de2a
Glauco Pater <>
2019-06-11 08:31:39 +0200

    update all packages fix Configurestore as logger must to be the last middleware (this was generating an issue on redux action) replace re-resize with rnd-react: now the resize works also with different zoom level.

--- src/ResizableImage/index.js
+++ src/ResizableImage/index.js
@@ -1,39 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Resizable from 're-resizable'
 import api from '../utils/api'
 import { calcSizeWithZoom, reCalcSizeWithZoom } from '../utils/calcZoom'
 import colors from '../constants/colors'
+import { Rnd } from 'react-rnd'
 
 const ResizableImage = ({ item, onSelect, selectedItemId, zoomLevel, onResize }) => {
   const height = calcSizeWithZoom(item.height, zoomLevel)
   const width = calcSizeWithZoom(item.width, zoomLevel)
   const x = calcSizeWithZoom(item.x, zoomLevel)
   const y = calcSizeWithZoom(item.y, zoomLevel)
+
   const onResizeStop = (e, direction, ref, d) =>
     onResize(item.id, {
-      ...item,
-      width: reCalcSizeWithZoom(width + d.width),
-      height: reCalcSizeWithZoom(height + d.height),
+      width: reCalcSizeWithZoom(width + d.width, zoomLevel),
+      height: reCalcSizeWithZoom(height + d.height, zoomLevel),
+      x: item.x,
+      y: item.y,
     })
 
   return (
-    <Resizable
-      defaultSize={{ width, height }}
+    <Rnd
       style={{
-        background: `url(${api.getImageUrl(item.path)})`,
+        background: `url(${api.getImageUrl(item.path)}) no-repeat`,
         backgroundSize: 'contain',
-        top: y,
-        left: x,
+        margin: 0,
+        height: '100%',
+        resize: 'both',
         border: selectedItemId === item.id ? `2px dashed ${colors.darkGrey}` : 'none',
         position: 'absolute',
       }}
+      size={{ width, height }}
+      position={{ x, y }}
       maxWidth={width * 3}
       maxHeight={height * 3}
       minHeight={height / 3}
       minWidth={width / 3}
       lockAspectRatio={true}
       onClick={() => onSelect(item.id)}
+      dragAxis={'none'}
+      bounds="window"
       onResizeStop={onResizeStop}
     />
   )
--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+import logger from 'redux-logger'
 
-const store = createStore(rootReducer, applyMiddleware(...[logger, thunk]));
+const store = createStore(rootReducer, applyMiddleware(...[thunk, logger]))
 
-export default store;
+export default store
