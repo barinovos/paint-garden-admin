5535362fa1f2e353d5e6fab05bffdb476c2b7311
Mirko Jovic <mirkojovic1996@gmail.com>
2019-12-18 01:06:38 +0100

    resize and drag

--- src/ResizableImage/index.js
+++ src/ResizableImage/index.js
@@ -4,11 +4,20 @@ import { calcSizeWithZoom, reCalcSizeWithZoom } from '../utils/calcZoom'
 import colors from '../constants/colors'
 import { Rnd } from 'react-rnd'
 
-const ResizableImage = ({ item, onSelect, selectedItemId, zoomLevel, onResize }) => {
+const ResizableImage = ({ item, onSelect, selectedItemId, zoomLevel, onResize, onDrop }) => {
   const height = calcSizeWithZoom(item.height, zoomLevel)
   const width = calcSizeWithZoom(item.width, zoomLevel)
-  const x = calcSizeWithZoom(item.posx, zoomLevel)
-  const y = calcSizeWithZoom(item.posy, zoomLevel)
+  let x = calcSizeWithZoom(item.posx, zoomLevel)
+  let y = calcSizeWithZoom(item.posy, zoomLevel)
+
+  const onDragStop = (e, d) => {
+    onDrop(item.id, {
+      width: item.width,
+      height: item.height,
+      posx: reCalcSizeWithZoom(d.x, zoomLevel),
+      posy: reCalcSizeWithZoom(d.y, zoomLevel),
+    })
+  }
 
   const onResizeStop = (e, direction, ref, d) =>
     onResize(item.id, {
@@ -18,11 +27,16 @@ const ResizableImage = ({ item, onSelect, selectedItemId, zoomLevel, onResize })
       posy: item.posy,
     })
 
+    const onDrag = (e, d) => {
+      x = d.x;
+      y = d.y;
+    }
+
   return (
     <Rnd
       style={{
         background: `url(${item.path}) no-repeat`,
-        backgroundSize: 'contain',
+        backgroundSize: width + 'px ' + height + 'px',
         margin: 0,
         height: '100%',
         resize: 'both',
@@ -30,16 +44,18 @@ const ResizableImage = ({ item, onSelect, selectedItemId, zoomLevel, onResize })
         position: 'absolute',
       }}
       size={{ width, height }}
-      position={{ x, y }}
+      position={{ x: x , y: y }}
       maxWidth={width * 3}
       maxHeight={height * 3}
       minHeight={height / 3}
       minWidth={width / 3}
       lockAspectRatio={true}
       onClick={() => onSelect(item.id)}
-      dragAxis={'none'}
+      dragAxis={'both'}
       bounds="window"
       onResizeStop={onResizeStop}
+      onDragStop={onDragStop}
+      onDrag={onDrag}
     />
   )
 }
@@ -50,6 +66,7 @@ ResizableImage.propTypes = {
   selectedItemId: PropTypes.string,
   zoomLevel: PropTypes.number,
   onResize: PropTypes.func,
+  onDrop: PropTypes.func,
 }
 
 export default ResizableImage
